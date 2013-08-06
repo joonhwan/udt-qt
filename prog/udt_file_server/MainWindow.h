ce9b5a7b659b843147150f6770fee8b1d9345b88
Joonhwan <joonhwan.lee@gmail.com>
2013-08-06 10:02:53 +0900

    initial update

---
+++ prog/udt_file_server/MainWindow.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow(QWidget* parent=0);
+    virtual ~MainWindow();
+signals:
+public slots:
+protected:
+};
---
+++ prog/udt_file_server/main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+
+int main(int argc, char** argv)
+{
+	QApplication app(argc, argv);
+	QCoreApplication::setOrganizationName("joonhwan");
+	QCoreApplication::setOrganizationDomain("www.joonhwan.org");
+	QCoreApplication::setApplicationName("udt-fileserver");
+	
+	return app.exec();
+}
